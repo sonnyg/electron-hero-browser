77292fce19906f778da0681638468eca8aa1928c
sonnyg <sonny@evagabond.com>
2016-09-08 10:09:30 -0700

    small spelling corrections and some added comments

--- main.js
+++ main.js
@@ -2,19 +2,22 @@
 
 const electron = require('electron');
 const app = electron.app;   // module to control application life
-const BrowserWindow = electron.BrowserWindow; // modeul to create native browser window
+const BrowserWindow = electron.BrowserWindow; // module to create native browser window
 var mainWindow = null;
 
+// don't close the app if on osx
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin') {
     app.quit();
   }
 });
 
+// create the main window now that the app module is ready
 app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 800, height: 600});
   mainWindow.loadURL(`file://${__dirname}/app/index.html`);
 
+  // clean up after ourselves
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
